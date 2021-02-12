90974d19a3832523a008d89b408bd7d31512e93a
ashourmoaaz <ashour.m@live.de>
2021-02-12 17:41:39 +0100

    rewording

--- train.py
+++ train.py
@@ -6,19 +6,17 @@ with open('intents.json') as f:
 
 all_words = []
 tags = []
-xy = [] # will hold patterns and tags
+xy = []  # will hold tokenized patterns and tags
 
 # loop over intents
-
 for intent in intents['intents']:
     tag = intent['tag']
     tags.append(tag)
     for pattern in intent['patterns']:
-        tokenized_words = tokenize(pattern)
-        all_words.extend(tokenized_words) # extend with array of words
-        xy.append((tokenized_words, tag))
-
-ignored_words = ['?', "!", '.', ',']
+        tokenized_pattern = tokenize(pattern)
+        all_words.extend(tokenized_pattern)  # extend with array of words
+        xy.append((tokenized_pattern, tag))
+ignored_characters = ['?', "!", '.', ',']
 
 # apply stemming to tokenized all_words list and excluding ignored_words
 all_words = [stem(word) for word in all_words if word not in ignored_words]
