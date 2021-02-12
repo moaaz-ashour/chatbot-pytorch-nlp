912cb2f1dffb6c35be13ef3f52c55baee686330b
ashourmoaaz <ashour.m@live.de>
2021-02-12 13:23:06 +0100

    implement tokenization

---
+++ nltk_utils.py
@@ -0,0 +1,15 @@
+import nltk
+
+# 1
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+
+# 2
+def stem(word):
+    pass
+
+
+# 3
+def bag_of_words(tokenized_sentence, all_words):
+    pass
