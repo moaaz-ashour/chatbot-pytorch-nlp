be1b2be9cd32ad91347f7016cbb0c06ef7a43794
ashourmoaaz <ashour.m@live.de>
2021-02-12 17:35:26 +0100

    implement bag_of_words method

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,4 +1,5 @@
 import nltk
+import numpy as np
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
 
@@ -14,4 +15,12 @@ def stem(word):
 
 # 3
 def bag_of_words(tokenized_sentence, all_words):
-    pass
+    # apply stemming
+    tokenized_sentence = [stem(word) for word in tokenized_sentence]
+    # initialize a bag with zeros for all words
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    # iterate over all words. If word is found, change to 1.0
+    for idx, word in enumerate(all_words):
+        if word in tokenized_sentence:
+            bag[idx] = 1.0 
+    return bag
