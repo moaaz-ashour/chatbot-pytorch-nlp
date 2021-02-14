ede5cbdf275555d0d7d8687dc08e86917d736521
ashourmoaaz <ashour.m@live.de>
2021-02-14 17:32:56 +0100

    calling super()

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 class NeuralNet(nn.Module):
     # feed forward neural net with 2 hidden layers
     def __init__(self, input_size, hidden_size, num_classes):
+        super().__init__()
         self.l1 = nn.Linear(input_size, hidden_size)
         self.l2 = nn.Linear(hidden_size, hidden_size)
         self.l3 = nn.Linear(hidden_size, num_classes)
