9c1af05347bf175b2f2551811a2ffa92580d9695
ashourmoaaz <ashour.m@live.de>
2021-02-14 17:19:17 +0100

    initialize Feedforward neural network: - 3 Linear layers (2x hidden) - ReLU - forward pass

--- model.py
+++ model.py
@@ -2,5 +2,20 @@ import torch
 import torch.nn as nn
 
 class NeuralNet(nn.Module):
-    def __init__(self):
-       pass
+    # feed forward neural net with 2 hidden layers
+    def __init__(self, input_size, hidden_size, num_classes):
+        self.l1 = nn.Linear(input_size, hidden_size)
+        self.l2 = nn.Linear(hidden_size, hidden_size)
+        self.l3 = nn.Linear(hidden_size, num_classes)
+        self.relu = nn.ReLU()
+
+        # forward pass
+        def forward(self, x):
+            out = self.l1(x)
+            out = self.relu(out)
+            out = self.l2(out)
+            out = self.relu(out)
+            out = self.l3(out)
+            out = self.relu(out)
+
+            return out
