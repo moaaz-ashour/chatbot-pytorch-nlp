cf4e355e8f7475e24f3c92ee9b35a2d9bc15fb98
ashourmoaaz <ashour.m@live.de>
2021-02-15 16:24:20 +0100

    return possible response if there is a tag match

--- chat.py
+++ chat.py
@@ -34,7 +34,28 @@ model.eval()
 bot_name = "Chat Bot"
 print("Let's Chat! type 'quit' to exit")
 
-While True:
+while True:
     sentence = input("You: ")
     if sentence == "quit":
         break
+
+    # tokenize sentence:
+    tok_sentence = tokenize(sentence)
+    # create an array of bag of words with tokenized sentence and words from the saved file
+    x = bag_of_words(tok_sentence, all_words)
+    # reshapeing to 1 row (i.e. 1 sample) and 1 column [54]
+    x = x.reshape(1, x.shape[0])
+    # convert it to torch tensor
+    x = torch.from_numpy()
+
+    # pass data as model input to get predictions
+    output = model(x)
+    # getting predictions
+    _, predicted = torch.max(output, dim=1)
+    # get actual tag
+    tag = tags[predicted.item()] # tags[class_label_number], e.g. greeting
+    # find corresponding intent by looping over intents and check if tags matches
+    for intent in intents["intents"]:
+        if tag == intent["tag"]:
+            # possible response
+            print(f"{bot_name}: {random.choice(intent["responses"])}")
