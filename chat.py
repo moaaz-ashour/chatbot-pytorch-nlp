c5c83561766cd58431cdea7df7dd5d2c1a1438a5
ashourmoaaz <ashour.m@live.de>
2021-02-15 15:59:06 +0100

    loading intents

--- chat.py
+++ chat.py
@@ -7,3 +7,6 @@ from nltk_utils import bag_of_words, tokenize
 
 # check GPU support
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+with open('intents.json') as f:
+    intents = json.load(f)
