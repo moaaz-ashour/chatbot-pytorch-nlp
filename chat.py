9023d8215385f0c5d0aee59a46f43cd098ad6b4a
ashourmoaaz <ashour.m@live.de>
2021-02-15 16:06:08 +0100

    create, load and set eval mode of model

--- chat.py
+++ chat.py
@@ -13,3 +13,18 @@ with open('intents.json') as f:
 
 FILE = "data.pth"
 data = torch.load(FILE)
+
+
+input_size = data['input_size']
+hidden_size = data['hidden_size']
+output_size = data['output_size']
+all_words = data['all_words']
+tags = data['tags']
+model_state_dict = data['model_state_dict']
+
+# create model
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+# load state dict 
+model.load_state_dict(model_state_dict)
+# set to evaluation mode
+model.eval()
